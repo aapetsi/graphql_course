941d3adb242c4a055730b0a40f23bd1a1aa2216c
aapetsi <apetsiampiah@gmail.com>
2020-09-26 08:05:30 +0000

    add subscriptions

---
+++ src/resolvers/subscription.js
@@ -0,0 +1,18 @@
+const subscription = {
+  count: {
+    subscribe(parent, args, { pubsub }, info) {
+      let count = 0
+
+      setInterval(() => {
+        count++
+        pubsub.publish('count', {
+          count,
+        })
+      }, 1000)
+
+      return pubsub.asyncIterator('count')
+    },
+  },
+}
+
+export { subscription as default }
